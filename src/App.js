df59f3c0ea7e0c8111ee2e58438bb8e2a9e9d4c8
Sameed1990 <111309698+Sameed1990@users.noreply.github.com>
2022-10-15 06:01:30 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -11,14 +11,36 @@ import Team from './componenets/Team';
 import Footer from './componenets/Footer';
 import Roadmap from './componenets/Roadmap';
 import MobNav from './componenets/MobNav';
+import scroll from './img/chevron-upwards-arrow.png'
 // import AOS from 'aos';
-
+import React,{useEffect} from 'react';
 function App() {
-  
+  useEffect(() => {
+    // 👇️ scroll to top on page load
+    window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+  }, []);
     // or simply just AOS.init();
     // AOS.init()
   return (
     <>
+     <button id='fixed'
+        onClick={() => {
+          window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+        }}
+        style={{
+          position: 'fixed',
+          padding: '1rem 2rem',
+          fontSize: '20px',
+          bottom: '40px',
+          right: '-160px',
+          backgroundColor: 'none',
+          color: 'none' ,
+          filter: 'brightness(0) invert(1)',
+          textAlign: 'center',
+        }}
+      >
+        <img src={scroll} alt="" />
+      </button>
     <NavBar  />
     <MobNav/>
     <Banner data-aos={"fade-up"} />
